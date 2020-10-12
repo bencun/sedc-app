8f908e3cdfbda2cfde8b2decc2cae603e5446a28
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-10-12 18:03:13 +0200

    Added a Vuex store.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+// import store from './store/index.js' -- indentical
 
 Vue.config.productionTip = false
 
 let app = new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 })
 app.$mount('#app')
