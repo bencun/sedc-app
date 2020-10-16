03f4b84bfcd1ab0fc12704f61b1babe9a10d0225
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-10-16 17:52:18 +0200

    Added Vue Router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
 // import store from './store/index.js' -- indentical
 
 Vue.config.productionTip = false
 
 let app = new Vue({
   store,
+  router,
   render: h => h(App)
 })
 app.$mount('#app')
